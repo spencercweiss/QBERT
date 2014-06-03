db7a684ff7580a7644f08f3c67b26fd7e9e96d2a
Kyler James Chase <kylerchase85@Kylers-MacBook-Air-2.local>
2014-06-03 19:03:53 -0400

    added box.java

---
+++ box.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class box {
+
+	private box parentL, parentR; left, right;
+
+	public int color;
+
+
+
+
+
+		
+	public void setRight(box b) {
+		right = b;
+		b.setParentL(this);
+	}
+
+	public void setLeft(box b) {
+		left = b;
+		b.setParentR(this);
+	}
+
+	public void setParentL(box b) {
+		ParentL = b;
+	}
+
+	public void setParentR(box b) {
+		ParentR = b;
+	}
+
+	public box getRight() {
+		return right;
+	}
+
+	public box getLeft() {
+		return left;
+	}
+
+	public box getParentL() {
+		return parentL;
+	}
+
+	public box getParentR(box b) {
+		return parentR;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
